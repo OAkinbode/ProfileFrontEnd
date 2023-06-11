5c2193a36e3e1b69d224250b3c2c6d1e205c43eb
OAkinbode <olusola.akinbode@ucalgary.ca>
2023-06-10 21:15:49 -0600

    further changes made

--- src/components/Textcontainer2.js
+++ src/components/Textcontainer2.js
@@ -3,7 +3,7 @@ import React from "react";
 function Skillslist(props) {
   return (
     <div className="px-6 py-6 w-full md:w-1/2">
-      <div className="w-full bg-gray-100 shadow-xl px-4 h-auto">
+      <div className="w-full bg-gray-100 shadow-xl px-4 h-full">
         <div className="border-b-2 border-gray-400 h-auto">
           <p className="font-sans text-2xl whitespace-normal text-left px-4 py-4 italic h-auto">
             {props.title}
